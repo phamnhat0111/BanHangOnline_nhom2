6a146e9371dc4bf4daae8b3a8969a876aa637b8f
Sam-Trung-Pham <samtrung0809@gmail.com>
2022-07-22 16:46:38 +0700

    code đăng kí đăng nhập

--- app/src/main/java/com/example/banhangonline/activity/activity/DangKiActivity.java
+++ app/src/main/java/com/example/banhangonline/activity/activity/DangKiActivity.java
@@ -1,14 +1,136 @@
 package com.example.banhangonline.activity.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
-import android.os.Bundle;import com.example.banhangonline.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.banhangonline.R;
+import com.example.banhangonline.activity.retrofit.ApiBanHang;
+import com.example.banhangonline.activity.retrofit.RetrofitClient;
+import com.example.banhangonline.activity.utils.Utils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class DangKiActivity extends AppCompatActivity {
-
+    EditText email,pass, repass,mobile,username;
+    AppCompatButton button;
+    ApiBanHang apiBanHang;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ki);
+        initView();
+        initControll();
+    }
+
+    private void initControll() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dangKi();
+            }
+        });
+    }
+
+    private void dangKi() {
+        String str_email = email.getText().toString().trim();
+        String str_pass = pass.getText().toString().trim();
+        String str_repass = repass.getText().toString().trim();
+        String str_user = username.getText().toString().trim();
+        String str_mobile = mobile.getText().toString().trim();
+        if(TextUtils.isEmpty(str_email)){
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập email",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(str_user)){
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập tên tài khoản",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(str_mobile)){
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập số điện thoại",Toast.LENGTH_SHORT).show();
+        }else if(TextUtils.isEmpty(str_pass)){
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập mật khẩu",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(str_repass)){
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập lại mật khẩu",Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            if(str_pass.equals(str_repass)){
+                firebaseAuth = FirebaseAuth.getInstance();
+                firebaseAuth.createUserWithEmailAndPassword(str_email,str_pass)
+                        .addOnCompleteListener(DangKiActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                                    if(user!=null){
+                                        postData(str_email,str_pass,str_user,str_mobile,user.getUid());
+                                    }
+                                } else {
+                                    Toast.makeText(getApplicationContext(),"Email đã tồn tại",Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }else{
+                Toast.makeText(getApplicationContext(),"Mật khẩu chưa khớp",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private  void postData(String str_email,String str_pass,String str_user,String str_mobile, String uid){
+        // post data
+        compositeDisposable.add(apiBanHang.dangKi(str_email,str_pass,str_user,str_mobile,uid)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        userModel -> {
+                            if(userModel.isSuccess()){
+                                Utils.user_current.setEmail(str_email);
+                                Utils.user_current.setPass(str_pass);
+                                Intent intent = new Intent(getApplicationContext(),DangNhapActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }else{
+                                Toast.makeText(getApplicationContext(),userModel.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        throwable -> {
+                            Toast.makeText(getApplicationContext(),throwable.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                ));
+
+    }
+
+    private void initView() {
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+
+        email=findViewById(R.id.email);
+        username=findViewById(R.id.username);
+        mobile=findViewById(R.id.mobile);
+        pass=findViewById(R.id.pass);
+        repass=findViewById(R.id.repass);
+        button=findViewById(R.id.btndangki);
+    }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/banhangonline/activity/retrofit/ApiBanHang.java
+++ app/src/main/java/com/example/banhangonline/activity/retrofit/ApiBanHang.java
@@ -2,6 +2,7 @@ package com.example.banhangonline.activity.retrofit;
 
 import com.example.banhangonline.activity.model.LoaiSpModel;
 import com.example.banhangonline.activity.model.SanPhamMoiModel;
+import com.example.banhangonline.activity.model.UserModel;
 
 
 import io.reactivex.rxjava3.core.Observable;
@@ -21,6 +22,22 @@ public interface ApiBanHang {
     Observable<SanPhamMoiModel> getSanPham(
             @Field("page") int page,
             @Field("loai") int loai
+    );
+
+    @POST("dangki.php")
+    @FormUrlEncoded
+    Observable<UserModel> dangKi(
+            @Field("email") String email,
+            @Field("pass") String pass,
+            @Field("username") String username,
+            @Field("mobile") String mobile,
+            @Field("uid") String uid
+    );
+    @POST("dangnhap.php")
+    @FormUrlEncoded
+    Observable<UserModel> dangNhap(
+            @Field("email") String email,
+            @Field("pass") String pass
     );
 
 }
